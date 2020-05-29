9d08a98b1ebbabe1adf5b36e8920c23e46533f01
wilsonjuniordeveloper <wilsonjuniordeveloper@bol.com.br>
2020-05-29 14:06:28 -0300

    remove import

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;1,300&display=swap');
-
     *{
         margin: 0;
         padding: 0;
